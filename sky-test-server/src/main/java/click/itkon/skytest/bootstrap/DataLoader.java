15768c89c4db8a5b2052a9ba1c37f16b5c665add
Sergii <sergendorfin@gmail.com>
2024-06-26 12:55:40 +0200

    Add Spring profiles. Add MySql docker connection to the app

--- sky-test-server/src/main/java/click/itkon/skytest/bootstrap/DataLoader.java
+++ sky-test-server/src/main/java/click/itkon/skytest/bootstrap/DataLoader.java
@@ -18,20 +18,26 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = User.builder()
-                .email("sam@mail.com")
-                .password("123")
-                .name(UserName.builder().firstName("Sam").lastName("Smith").build())
-                .externalProjects(List.of(
-                        ExternalProject.builder()
-                                .name("Project 1")
-                                .description("First important project").build(),
-                        ExternalProject.builder()
-                                .name("Project 2")
-                                .description("Second important project").build()
-                ))
-                .build();
-        user.getExternalProjects().forEach(ep -> ep.setUser(user));
-        userRepository.save(user);
+
+        String email = "sam@mail.com";
+
+        if (!userRepository.existsByEmail(email)) {
+            User user = User.builder()
+                    .email(email)
+                    .password("123")
+                    .name(UserName.builder().firstName("Sam").lastName("Smith").build())
+                    .externalProjects(List.of(
+                            ExternalProject.builder()
+                                    .name("Project 1")
+                                    .description("First important project").build(),
+                            ExternalProject.builder()
+                                    .name("Project 2")
+                                    .description("Second important project").build()
+                    ))
+                    .build();
+            user.getExternalProjects().forEach(ep -> ep.setUser(user));
+
+            userRepository.save(user);
+        }
     }
 }
--- sky-test-server/src/test/java/click/itkon/skytest/services/UserServiceImplIT.java
+++ sky-test-server/src/test/java/click/itkon/skytest/services/UserServiceImplIT.java
@@ -10,11 +10,13 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class UserServiceImplIT {
 
     @Autowired
