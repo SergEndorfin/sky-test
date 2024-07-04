862097a55fe1dd066feb6b67ee38c966f890ee18
Sergii <sergendorfin@gmail.com>
2024-07-04 13:36:54 +0200

    Update exception handling

--- sky-test-server/src/main/java/click/itkon/skytest/exceptions/NotFoundException.java
+++ sky-test-server/src/main/java/click/itkon/skytest/exceptions/NotFoundException.java
@@ -1,11 +1,5 @@
 package click.itkon.skytest.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import static click.itkon.skytest.exceptions.NotFoundException.ERROR_MESSAGE;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = ERROR_MESSAGE)
 public class NotFoundException extends RuntimeException {
 
     public static final String ERROR_MESSAGE = "Source not found";
